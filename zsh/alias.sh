32945260d1122a0bddcea8c6c52726bb81431a55
eduardoquea3 <eduardoquea3@gmail.com>
2026-01-17 21:42:42 -0500

    feat: initial commit
    - Add configurations of different tools and apps

---
+++ zsh/alias.sh
@@ -0,0 +1,18 @@
+alias ss='source ~/.zshrc'
+alias ns='nvim ~/.zshrc'
+alias nsa='nvim ~/.config/zsh/alias.sh'
+alias cls='clear'
+
+alias ls='eza --icons'
+alias la='eza --icons -a'
+
+alias dev='just dev'
+
+alias lg='lazygit'
+alias ld='lazydocker'
+
+alias i='impala'
+alias bt='bluetui'
+alias wm='wiremix'
+
+alias pd='podman'
