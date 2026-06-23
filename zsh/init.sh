676d28b02e6e3ab38a38e781647301e07f91f9df
eduardoquea3 <eduardoquea3@gmail.com>
2026-06-23 17:24:44 -0500

    feat(zsh): add Android SDK env vars and PATH
    Export ANDROID_HOME pointing to $HOME/Android/Sdk and prepend emulator and platform-tools to PATH for Android development.

--- zsh/init.sh
+++ zsh/init.sh
@@ -3,6 +3,9 @@
 eval "$(starship init zsh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME=""
